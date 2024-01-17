9c611691f7079d45258fcfc33d67f318215128e5
Jhonathan Amaral <jhon14amaral1@gmail.com>
2024-01-16 22:11:36 -0300

    :recycle: refactor: Refactoring Contact Repository with Postgres Database

--- src/app/repositories/ContactsRespository.js
+++ src/app/repositories/ContactsRespository.js
@@ -7,7 +7,10 @@ class ContactsRepository {
   async findAll(orderBy = "ASC") {
     const direction = orderBy.toUpperCase() === "DESC" ? "DESC" : "ASC";
     const rows = await db.query(
-      `SELECT * FROM contacts ORDER BY name ${direction}`
+      `SELECT contacts.*,categories.name as category_name
+       FROM contacts
+       LEFT JOIN categories ON categories.id = contacts.category_id
+       ORDER BY contacts.name ${direction}`
     );
     return rows;
   }
